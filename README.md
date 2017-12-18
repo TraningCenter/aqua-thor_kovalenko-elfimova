d01cdbaa5e9c5082bde391acb097cec5528a4f83
HeyTricky <kovnata95@mail.ru>
2017-12-18 15:06:46 +0300

    init project structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # aqua-thor_kovalenko-elfimova
+## Клеточный автомат. Акватор
+
+Чтобы запустить программу необходимо выполнить следующую команду:
+mvn clean install exec:java
---
+++ src/main/java/com/netcracker/unc/Main.java
@@ -0,0 +1,12 @@
+package com.netcracker.unc;
+
+
+/**
+ * App entry point
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
